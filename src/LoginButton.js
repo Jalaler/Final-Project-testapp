92857e634a7d46942d18aee04bca39ced135825f
Memorisza <aut14030@hotmail.com>
2021-09-26 20:57:07 +0700

    [HOTFIX] URL

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import backendURL from "./URL";
+import backendURL from "./URL.js";
 
 const useStyles = makeStyles((theme) => ({
     modal: {
