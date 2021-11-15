c0d1396a86a1232535152b1e2eea4aebbc0f8c6a
Raninger <44706558+Jalaler@users.noreply.github.com>
2021-11-15 15:14:35 +0700

    [Format] HistoryPage

--- src/HistoryPage.js
+++ src/HistoryPage.js
@@ -15,52 +15,52 @@ function HistoryPage() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        axios.get(backendURL+'/api/reviews/history/6124839e65cfb652d8df2b67')
-        .then(res => {
-            if(res.data.length > 0){
-                setData(res.data)
-            }
-        })
-        .catch(err => console.log(err))
-    },[]);
+        axios.get(backendURL + '/api/reviews/history/6124839e65cfb652d8df2b67')
+            .then(res => {
+                if (res.data.length > 0) {
+                    setData(res.data)
+                }
+            })
+            .catch(err => console.log(err))
+    }, []);
 
     const historyList = () => {
-        return data.map( currentPost => {
-            return <PostBox data={currentPost}/>
+        return data.map(currentPost => {
+            return <PostBox data={currentPost} />
         })
     }
 
-        return (
-            <div className="HistoryPage">
-                <Navbar />
-                <Scroll showBelow={250} />
-                <div class="absolute z-40 hidden md:block md:w-16 lg:w-20 pt-44">
-                    <img src={IMAGES.BigAnt} class="md:pt-5 lg:pt-1 md:ml-36 lg:ml-60" />
-                </div>
-                <div class="absolute z-40 hidden md:block md:w-16 lg:w-20 pt-52 md:mr-52 lg:mr-80 right-0">
-                    <img src={IMAGES.SmallAnt} class="md:pt-8 lg:pt-5" />
-                </div>
-                <div class="absolute z-40 hidden md:block md:w-16 lg:w-20 pt-52 md:mr-32 lg:mr-60 right-0">
-                    <img src={IMAGES.SmallAnt} class="md:pt-8 lg:pt-5" />
-                </div>
-                <div class="relative bg-no-repeat bg-top bg-auto z-0 hidden md:block">
-                    <img src={IMAGES.BannerBackground3} id="historybannerbg" class="opacity-100 absolute w-full mt-6" />
-                </div>
-                <div class="relative bg-no-repeat bg-top bg-auto z-0 md:hidden">
+    return (
+        <div className="HistoryPage">
+            <Navbar />
+            <Scroll showBelow={250} />
+            <div class="absolute z-40 hidden md:block md:w-16 lg:w-20 pt-44">
+                <img src={IMAGES.BigAnt} class="md:pt-5 lg:pt-1 md:ml-36 lg:ml-60" />
+            </div>
+            <div class="absolute z-40 hidden md:block md:w-16 lg:w-20 pt-52 md:mr-52 lg:mr-80 right-0">
+                <img src={IMAGES.SmallAnt} class="md:pt-8 lg:pt-5" />
+            </div>
+            <div class="absolute z-40 hidden md:block md:w-16 lg:w-20 pt-52 md:mr-32 lg:mr-60 right-0">
+                <img src={IMAGES.SmallAnt} class="md:pt-8 lg:pt-5" />
+            </div>
+            <div class="relative bg-no-repeat bg-top bg-auto z-0 hidden md:block">
+                <img src={IMAGES.BannerBackground3} id="historybannerbg" class="opacity-100 absolute w-full mt-6" />
+            </div>
+            <div class="relative bg-no-repeat bg-top bg-auto z-0 md:hidden">
                 <div id="historybannerbg2" class="bg-yellow-500 opacity-10 absolute w-full">
                 </div>
             </div>
-                <div class="z-40 relative">
-                    <div class="pt-36 text-center font-bold text-3xl sm:text-3xl lg:text-4xl">
-                        My review history
-                    </div>
+            <div class="z-40 relative">
+                <div class="pt-36 text-center font-bold text-3xl sm:text-3xl lg:text-4xl">
+                    My review history
                 </div>
-                <div class="pt-36">
-                    {historyList()}
-                </div>
-                <Footer />
             </div>
-        );
+            <div class="pt-36">
+                {historyList()}
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default HistoryPage;
