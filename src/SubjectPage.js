41c62afb18405b04a27c9e5b3ec8207b02be7fe2
Raninger <44706558+Jalaler@users.noreply.github.com>
2021-11-16 17:02:13 +0700

    [Update] SubjectList + SubjectPage
    1. SubjectList - padding + remove star
    2. SubjectPage - put footer to bottom

--- src/SubjectPage.js
+++ src/SubjectPage.js
@@ -7,16 +7,19 @@ import SubjectListType2 from "./SubjectListType2";
 
 function SubjectPage() {
     return (
-        <div className="SubjectPage">
+        <div className="SubjectPage" class="flex flex-col h-screen">
             <Navbar />
             <SearchBanner />
             <Scroll showBelow={250} />
-            <div class="font-bold text-2xl lg:text-3xl pt-36 md:pt-40 pb-4 md:pb-8 flex justify-start md:justify-center w-1/2 ml-10 sm:ml-20 md:ml-0">
-                Subject
-            </div>
-            <SubjectListType1 />
-            {/* <SubjectListType2 /> */}
-            <FooterEmpty />
+            <main class="flex-grow">
+                <div class="font-bold text-2xl lg:text-3xl pt-36 md:pt-40 pb-4 md:pb-8 flex justify-start md:justify-center w-1/2 ml-10 sm:ml-20 md:ml-0">
+                    Subject
+                </div>
+                <SubjectListType1 />
+            </main>
+            <footer>
+                <FooterEmpty />
+            </footer>
         </div>
     );
 }
