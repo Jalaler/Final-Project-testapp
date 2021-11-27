1c8569b51eaacfc6127afeaae29844014478d287
OhmSupachai <henryohohm@gmail.com>
2021-11-27 22:19:11 +0700

    log out

--- src/Navbar.js
+++ src/Navbar.js
@@ -25,13 +25,21 @@ window.addEventListener('DOMContentLoaded', () => {
 function Navbar() {
     const [currentUser, setCurrentUser] = useState({});
     useEffect(() => {
-            axios.get(backendURL + '/api/users/current', { withCredentials: true })
+        axios.get(backendURL + '/api/users/current', { withCredentials: true })
             .then(res => {
                 setCurrentUser(res.data)
-              
             })
             .catch(err => console.log(err))
     }, []);
+
+    function logOut() {
+        axios.get(backendURL + '/api/auth/logout', { withCredentials: true })
+            .then(res => {
+                setCurrentUser(res.data)
+            })
+            .catch(err => console.log(err))
+        window.location = '/'
+    }
 
     return (
         <nav class="bg-white shadow-md sm:shadow-md md:shadow-md lg:shadow-lg fixed right-0 left-0 z-50">
@@ -45,21 +53,25 @@ function Navbar() {
                     <li><NavLink exact activeClassName="active" to='/'>Home</NavLink></li>
                     <li><NavLink activeClassName="active" to='/subject'>Subject</NavLink></li>
                     <li><NavLink activeClassName="active" to='/review/GEN111'>Review</NavLink></li>
-                    <li><NavLink activeClassName="active" to={'/history/'+ currentUser._id}>History</NavLink></li>
+                    <li><NavLink activeClassName="active" to={'/history/' + currentUser._id}>History</NavLink></li>
                 </ul>
-                <LoginButton />
-                <div class="flex hidden">
-                    <button class="" id="btn" aria-describedby="tooltip" data-tooltip-text="ohm.supachai@mail.kmutt.ac.th">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 opacity-50" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
-                        </svg>
-                    </button>
-                    <div class="text-md mt-20 right-2 sm:right-8 md:right-16 lg:right-8 xl:right-auto bg-gray-700 text-white py-2 px-4 absolute rounded-3xl bg-opacity-50 shadow-lg hidden" id="tooltip" role="tooltip">
+
+                {!currentUser && <LoginButton />}
+                {currentUser &&
+                    // <div class="flex hidden">
+                    <div class="flex">
+                        <button class="" id="btn" aria-describedby="tooltip" data-tooltip-text={currentUser.displayName}>
+                            <svg xmlns={currentUser.image} class="h-6 w-6 opacity-50" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                        <div class="text-md mt-20 right-2 sm:right-8 md:right-16 lg:right-8 xl:right-auto bg-gray-700 text-white py-2 px-4 absolute rounded-3xl bg-opacity-50 shadow-lg hidden" id="tooltip" role="tooltip">
+                        </div>
+                        <div class="flex items-center pl-4">
+                            <a href="#" class="py-1 px-4 font-semibold text-yellow-500 bg-white border-2 border-yellow-300 rounded-full shadow-md hover:bg-yellow-50 transition duration-300" onClick={logOut}>Logout</a>
+                        </div>
                     </div>
-                    <div class="flex items-center pl-4">
-                        <a href="#" class="py-1 px-4 font-semibold text-yellow-500 bg-white border-2 border-yellow-300 rounded-full shadow-md hover:bg-yellow-50 transition duration-300">Logout</a>
-                    </div>
-                </div>
+                }
             </div>
         </nav>
     );
